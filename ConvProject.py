fe13636899449049194971953da2bf2f67db459e
kurcher <kurch2950@gmail.com>
2025-01-22 23:29:18 +0300

    new_conv

--- ConvProject.py
+++ ConvProject.py
@@ -15,41 +15,28 @@ def get_usd_to_uah_rate():
     except Exception as e:
         print(f"Error fetching exchange rate: {e}")
         return None
-
-
-
-def conv(course, count_of_grn, count_of_dollars, to_grn):
-    if to_grn == 'to_grn':
-
-     conv_from_d_to_g = int(float(count_of_dollars)*course)
-     rounded_1 = round(conv_from_d_to_g, 2)
-     print(rounded_1)
-    else:
-        conv_from_g_to_d = (float(count_of_grn)/course)
-        rounded_2 = round(conv_from_g_to_d, 2)
-
-        print(rounded_2)
-convertor_of_value = True
-while convertor_of_value:
-    def convertor():
-        rate = get_usd_to_uah_rate()
+def conv_to_uah(rate, count_of_usd):
+    conv_from_usd_to_uah = int(float(count_of_usd)*rate)
+    rounded_1 = round(conv_from_usd_to_uah, 2)
+    print(rounded_1)
+def conv_to_usd(rate, count_of_uah):
+    conv_from_uah_to_usd = (float(count_of_uah)/rate)
+    rounded_2 = round(conv_from_uah_to_usd, 2)
+    print (rounded_2)
+def convertor():
+        rate_of_usd = get_usd_to_uah_rate()
         print ('Welcome to the course convertor grn to dollars and dollars to grn')
-        to_grn_or_to_dollar_or_course = input(str('Type you want to convert to grn/dollar or you just to know the course. Type "off" to off the course convertor'))
-        if to_grn_or_to_dollar_or_course == 'off':
-            global convertor_of_value
-            convertor_of_value = False
-        elif to_grn_or_to_dollar_or_course == 'to_grn':
-            dollars = input('Type here how much dollars u want to convert to grn')
-            grns = 0
-            converted = conv(rate, grns, dollars, to_grn_or_to_dollar_or_course)
-        elif to_grn_or_to_dollar_or_course == 'course':
-            print(f'The NBU information says that the course now is {rate}')
+        to_uah_or_to_usd_or_course = input(str('Type you want to convert to uah/usd or you just to know the course. Type "off" to off the course convertor'))
+        if to_uah_or_to_usd_or_course == 'to uah':
+            usd = input('Type here how much dollars u want to convert to grn')
+            uah = 0
+            converted_to_uah = conv_to_uah(rate_of_usd, usd)
+        elif to_uah_or_to_usd_or_course == 'off':
+            return None
+        elif to_uah_or_to_usd_or_course == 'course':
+            print(f'The NBU information says that the course now is {rate_of_usd}')
         else:
-            grns = input('Type here how much grn u want to convert to dollars')
-            dollars = 0
-            converted = conv(rate, grns, dollars, to_grn_or_to_dollar_or_course)
-
-
-
-
-    convertor()
+            uah = input('Type here how much grn u want to convert to dollars')
+            usd = 0
+            converted_to_usd = conv_to_usd(rate_of_usd, uah)
+convertor()
